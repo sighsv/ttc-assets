dd98569a0a3135a638d4d09fcc2442d3425b1368
sighsv <228826370+sighsv@users.noreply.github.com>
2025-09-05 14:47:01 -0700

    Move die generator function into standalone script

--- src/Bits and Pieces/dice/generate.sh
+++ src/Bits and Pieces/dice/generate.sh
@@ -2,20 +2,13 @@
 cd "$(dirname "$(realpath "$0")")"
 
 compile() {
-    oid="$1"
-    ofile="$2"
-    shift
-    shift
-    echo Generating $ofile.glb
-    inkscape dice.svg --export-width=512 --export-type=png --export-filename="$oid.png" --export-id="$oid" 2>/dev/null
-    ../../cuboid.py -b -s -i "$oid.png" -o "$ofile.glb" "$@" --name "$ofile.material"
-    rm "$oid.png"
+    echo Generating $2.glb
+    ../../diegen.sh dice.svg "$@"
 }
 
 d="-x 1.6 -y 1.6 -z 1.6"
 
 compile perfect_die 'Perfect Die' -x 1.9685 -y 1.9685 -z 1.9685
-
 compile doubling_cube 'Doubling Cube' $d
 compile sicherman_low 'Sicherman Die Low' $d
 compile sicherman_high 'Sicherman Die High' $d
---
+++ src/diegen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+temp=$(mktemp)
+
+here=$(realpath "$0")
+heredir=$(dirname "$here")
+cubegen="$heredir/cuboid.py"
+
+svg="$1"
+sid="$2"
+name="$3"
+shift
+shift
+shift
+inkscape "$svg" --export-width=512 --export-type=png --export-filename="$temp.png" --export-id="$sid" 2>/dev/null
+python3 "$cubegen" -b -s -i "$temp.png" -o "$name.glb" "$@" --name "$name"
+rm -f "$temp.png"
