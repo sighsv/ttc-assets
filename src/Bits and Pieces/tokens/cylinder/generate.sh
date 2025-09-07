c91ff0aba5f94224e56ed3f9b63ef40c26bb7cbe
sighsv <228826370+sighsv@users.noreply.github.com>
2025-09-07 11:46:31 -0700

    Add poker chips

--- src/Bits and Pieces/tokens/cylinder/generate.sh
+++ src/Bits and Pieces/tokens/cylinder/generate.sh
@@ -1,15 +1,27 @@
-#!/bin/sh
-cd "$(dirname "$(realpath "$0")")"
+#!/bin/bash
+canonicalize
 
-outdir="../../../../assets/Bits and Pieces/tokens/cylinder"
+outdir="$ASSETS/Bits and Pieces/tokens/cylinder"
 mkdir -p "$outdir"
 
 render() {
-    inkscape flippers.svg --export-width=512 --export-type=png --export-filename="$outdir/$1.png" --export-id="$1" 2>/dev/null
+    echo Generating $3.png
+    f="$(inkscape_png.sh "$1" w512 "$2")"
+    mv "$f" "$outdir/$3.png"
 }
 
-render reversi
-render red_yellow
-render green_blue
+render flippers.svg reversi reversi
+render flippers.svg red_yellow red_yellow
+render flippers.svg green_blue green_blue
+
+render pokerchips.svg one "Poker Chip 1"
+render pokerchips.svg five "Poker Chip 5"
+render pokerchips.svg ten "Poker Chip 10"
+render pokerchips.svg twenty_five "Poker Chip 25"
+render pokerchips.svg one_hundred "Poker Chip 100"
+render pokerchips.svg five_hundred "Poker Chip 500"
+render pokerchips.svg one_thousand "Poker Chip 1000"
+render pokerchips.svg five_thousand "Poker Chip 5000"
+render pokerchips.svg ten_thousand "Poker Chip 10000"
 
 cp -v *.cfg "$outdir"
